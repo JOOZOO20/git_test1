a1ca87002499922ab661f1872571e1e76ef48e6f
JOOZOO20 <chaheeju01@gmail.com>
2024-09-15 22:24:29 +0900

    메인 메소드 수정

--- Hello.java
+++ Hello.java
@@ -1,9 +1,12 @@
 package git_test2;
 
 public class Hello {
+    
+    String message = "안녕하세요";
 
     public void say() {
-        System.out.println("hello world");
+        System.out.println(message);
     }
 
 }
+
